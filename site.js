89844135f2311f884d4ae6a1db0df6356c8641d6
JackElvinsson <jackelvinsson@gmail.com>
2023-03-23 03:56:41 +0100

    fixedMapSize

--- site.js
+++
@@ -1,5 +0,0 @@
-// Add an event listener to the "Products" link
-const productsLink = document.querySelector('#products-button');
-productsLink.addEventListener('click', () => {
-    fetchAllProducts('products');
-});
