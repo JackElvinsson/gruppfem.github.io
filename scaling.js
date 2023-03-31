23ffa9f35b433004e6cc04112acda8e1b27ba752
JackElvinsson <jackelvinsson@gmail.com>
2023-03-31 15:08:28 +0200

    fix

--- scaling.js
+++ scaling.js
@@ -10,8 +10,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
         } else if (window.innerWidth < 700) {
             document.getElementById('electronics').textContent = 'Electronics';
-            document.getElementById('mens').textContent = 'Men\'s Clothing';
-            document.getElementById('womens').textContent = 'Women\'s Clothing';
+            document.getElementById('mens').textContent = 'Men\'s';
+            document.getElementById('womens').textContent = 'Women\'s';
             document.getElementById('jewelry').textContent = 'Jewelry';
 
         } else {
